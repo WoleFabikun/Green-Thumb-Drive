cb2c2d8be8ddc8b64dfe1cb36b9cdfc77366877c
Wole Fabikun <wole359@gmail.com>
2024-03-13 23:21:53 -0400

    shadcn

---
+++ client/src/components/userInput.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+export function InputWithButton() {
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="email" placeholder="Ask about your plant" />
+      <Button type="submit">Send</Button>
+    </div>
+  )
+}
